853f1934683cb3bedc020a983d9e653a1f059b6d
justinoriola <justinoriola@yahoo.co.uk>
2024-02-08 17:03:24 +0000

    amend env phone data

--- notification_handler.py
+++ notification_handler.py
@@ -6,8 +6,9 @@ load_dotenv(dotenv_path='.env')
 
 TWILIO_SID=os.environ["TWILIO_ACCOUNT_SID"]
 TWILIO_AUTH_TOKEN=os.environ["TWILIO_AUTH_TOKEN"]
-TWILIO_VIRTUAL_NUMBER="+441223658283"
-TWILIO_VERIFIED_NUMBER="+447459555061"
+TWILIO_VIRTUAL_NUMBER=os.getenv("TWILIO_PHONE_NUMBER")
+TWILIO_VERIFIED_NUMBER=os.getenv("TWILIO_VERIFIED_NUMBER")
+TWILIO_WHATSAPP_NUMBER=os.getenv("TWILIO_WHATSAPP_NUMBER")
 
 class NotificationHandler:
     def __init__(self):
@@ -16,9 +17,9 @@ class NotificationHandler:
     def send_sms(self, message):
         message = self.client.messages \
             .create(
-            from_='whatsapp:+14155238886',
+            from_=f'whatsapp:{TWILIO_WHATSAPP_NUMBER}',
             body=message,
-            to='whatsapp:+447459555061'
+            to=f'whatsapp:{TWILIO_VERIFIED_NUMBER}'
         )
 
         print(f'\nWhatsApp notification sent successfully: {message.sid}')
